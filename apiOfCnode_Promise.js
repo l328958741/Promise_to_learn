0b07d773da0030755f1735a540d894ca998d2b4a
Liuzc <328958741@qq.com>
2018-05-19 22:50:47 +0800

    Update apiOfCnode_Promise.js
    与promise写法对比

--- apiOfCnode_Promise.js
+++ apiOfCnode_Promise.js
@@ -16,6 +16,21 @@ function readFirstName(){
 }
 
 function readPersonInfo(name){
+    return new Promise(function(resolve, reject){
+        request('https://cnodejs.org/api/v1/user/' + name, function(error, response, body){
+        if(error){
+            reject(error);
+        }
+
+        let info = JSON.parse(response.body).data
+        console.log('蜜汁顺序：~~~~'+info);//这里打印出来为什么是“object”, 而不是具体的数据呢？因为前面加了字符串，并用“+”操作符连接了
+
+        // resolve(info);
+    })
+    })
+}
+
+function readPersonInfo2(name){
     request('https://cnodejs.org/api/v1/user/' + name, function(error, response, body){
         if(error){
             console.log(error);
@@ -23,7 +38,7 @@ function readPersonInfo(name){
 
         let info = JSON.parse(response.body).data
         console.log(info);
-    })
-}
+    }
+)}
 
-readFirstName().then(readPersonInfo);
+readFirstName().then(readPersonInfo).catch(console.log);
