326fc575f99f18d8b46d298acea35b6d9bc96fc0
Liuzc <328958741@qq.com>
2018-05-19 22:51:52 +0800

    与promise写法对比

--- readAndWrite_example_v2.js
+++ readAndWrite_example_v2.js
@@ -9,10 +9,31 @@ let readDir = util.promisify(fs.readdir);
 
 function writeDir(path){
     return function(content){
-        return util.promisify(fs.writeFile(path,content));
+        return util.promisify(fs.writeFile)(path,content);
     }
 }
 
-readDir(dirPath).then(R.filter(R.test(/.json/))).then(console.log);
+
+readDir(dirPath).then(R.filter(R.test(/.json/))).then(writeDir(outputPath)).catch(console.log);
+
+
+// /*------------------原始写法------------------------*/
+// readDir(dirPath).then(files => {
+//     let names = files.map(item => {
+//          if(item.endsWith('.json')){
+//              return item
+//          }
+//     })
+//     // console.log(names);
+
+//     return names;
+// }).then(data => {
+//     function checkArry(value){
+//         return value != undefined;
+//     }
+
+//     let result = data.filter(checkArry);
+//     console.log(result);
+// }).then(writeDir(outputPath)).catch(console.log)
 
 
